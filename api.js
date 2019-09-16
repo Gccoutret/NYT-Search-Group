1cd301c90de953024e5613c7b941c45d97c1aa80
race-briggs <race.briggs@gmail.com>
2019-09-16 13:35:03 -0500

    added onclick function

--- api.js
+++ api.js
@@ -3,6 +3,11 @@ $(document).ready(function(){
   var key= "SDJXzizDrGMNzTGFfnYY0w1FqDEoGdQf";
   var url= "https://api.nytimes.com/svc/search/v2/articlesearch.json?q="+topic+"&api-key="+key;
   var topic;
+
+  $('#search-btn').on('click', function(event){
+    event.preventDefault;
+    topic = $('#query').val();
+  })
   
     $.ajax({
       method: "GET",
