e91c801fce49a767adb70aefba97059d87672740
GCout <Gccoutret@gmail.com>
2019-09-16 13:32:06 -0500

    added ajax request

--- api.js
+++ api.js
@@ -1,5 +1,18 @@
 $(document).ready(function(){
 
+  var key= "SDJXzizDrGMNzTGFfnYY0w1FqDEoGdQf";
+  var url= "https://api.nytimes.com/svc/search/v2/articlesearch.json?q="+topic+"&api-key="+key;
+  var topic;
+  
+    $.ajax({
+      method: "GET",
+      url: url,
+      
+    }).then(function(response){
+      console.log(response)
+    })
+    
+
   
 
 
