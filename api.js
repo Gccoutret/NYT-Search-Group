fe0e5fe7d0022fae672d6716d419135f519b54a4
race-briggs <race.briggs@gmail.com>
2019-09-16 13:43:57 -0500

    added search function

--- api.js
+++ api.js
@@ -1,21 +1,29 @@
 $(document).ready(function(){
 
   var key= "SDJXzizDrGMNzTGFfnYY0w1FqDEoGdQf";
-  var url= "https://api.nytimes.com/svc/search/v2/articlesearch.json?q="+topic+"&api-key="+key;
-  var topic;
 
-  $('#search-btn').on('click', function(event){
-    event.preventDefault;
-    topic = $('#query').val();
-  })
-  
+  $('#search-btn').on('click', function(){
+    event.preventDefault();
+
+    var topic = $('#query').val();
+
+    console.log(topic);
+
+    var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + topic + "&api-key=" + key;
+
+    console.log(url);
+    
     $.ajax({
       method: "GET",
       url: url,
-      
     }).then(function(response){
       console.log(response)
     })
+
+
+  })
+  
+
     
 
   
